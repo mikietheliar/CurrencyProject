c265e9ba0a83164d48a44ec4ba8a97ce5362cc30
mikietheliar <167641667+mikietheliar@users.noreply.github.com>
2024-05-14 21:45:53 +0300

    added Initial file

---
+++ Initial.py
@@ -0,0 +1,8 @@
+class Wallet:
+    def __init__(self, usd: str, rur: str, eu: str, gbp: str, fr: str) -> None:
+        self.currencies = dict()
+        self.currencies['usd'] = usd
+        self.currencies['rur'] = rur
+        self.currencies['eu'] = eu
+        self.currencies['gbp'] = gbp
+        self.currencies['fr'] = fr
