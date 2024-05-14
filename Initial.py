83670703a2f0ec96bd4b4a5648a0815870944811
mikietheliar <167641667+mikietheliar@users.noreply.github.com>
2024-05-14 23:13:35 +0300

    added exchange rates for def __init__ added convertion method (have no idea how it works)

--- Initial.py
+++ Initial.py
@@ -1,11 +1,17 @@
 class Wallet:
-    def __init__(self, usd: str, rur: str, eu: str, gbp: str, fr: str) -> None:
+    def __init__(self, usd: str, rur: str, eu: str, gbp: str, cad: str) -> None:
         self.currencies = dict()
         self.currencies['usd'] = usd
         self.currencies['rur'] = rur
         self.currencies['eu'] = eu
         self.currencies['gbp'] = gbp
-        self.currencies['fr'] = fr
+        self.currencies['cad'] = cad
+        self.rates = dict()
+        self.rates['usd'] = {'usd': 1, 'rur': 0.011, 'eu': 1.1, 'gbp': 1.2, 'cad': 0.7}
+        self.rates['rur'] = {'usd': 90, 'rur': 1, 'eu': 100, 'gbp': 120, 'cad': 60}
+        self.rates['eu'] = {'usd': 0.95, 'rur': 0.01, 'eu': 1, 'gbp': 1.15, 'cad': 0.6}
+        self.rates['gbp'] = {'usd': 0.7, 'rur': 0.008, 'eu': 0.85, 'gbp': 1, 'cad': 0.5}
+        self.rates['cad'] = {'usd': 1.2, 'rur': 0.016, 'eu': 1.3, 'gbp': 2, 'cad': 1}
 
     def __str__(self) -> str:
         return str(self.currencies)
@@ -19,6 +25,10 @@ class Wallet:
         else:
             self.currencies[currency] -= amount
 
-
-
+    def convert_currency(self, from_currency: str, to_currency: str, amount: int) -> None:
+        if self.currencies[from_currency] < amount:
+            print('Not enough money')
+        else:
+            self.currencies[to_currency] += amount * self.rates[to_currency][from_currency]
+            self.currencies[from_currency] -= amount
 
