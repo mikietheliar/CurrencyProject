7cd69ad316f1083a81568845d44d9c94e30b9981
mikietheliar <167641667+mikietheliar@users.noreply.github.com>
2024-05-15 21:38:04 +0300

    added checking method before buying goods

--- Initial.py
+++ Initial.py
@@ -7,11 +7,11 @@ class Wallet:
         self.currencies['gbp'] = gbp
         self.currencies['cad'] = cad
         self.rates = dict()
-        self.rates['usd'] = {'usd': 1, 'rur': 0.011, 'eu': 1.1, 'gbp': 1.2, 'cad': 0.7}
-        self.rates['rur'] = {'usd': 90, 'rur': 1, 'eu': 100, 'gbp': 120, 'cad': 60}
-        self.rates['eu'] = {'usd': 0.95, 'rur': 0.01, 'eu': 1, 'gbp': 1.15, 'cad': 0.6}
-        self.rates['gbp'] = {'usd': 0.7, 'rur': 0.008, 'eu': 0.85, 'gbp': 1, 'cad': 0.5}
-        self.rates['cad'] = {'usd': 1.2, 'rur': 0.016, 'eu': 1.3, 'gbp': 2, 'cad': 1}
+        self.rates['usd'] = {'usd': 1, 'rur': 0.01, 'eu': 1.1, 'gbp': 1.3, 'cad': 0.7}
+        self.rates['rur'] = {'usd': 90, 'rur': 1, 'eu': 100, 'gbp': 120, 'cad': 67}
+        self.rates['eu'] = {'usd': 0.9, 'rur': 0.01, 'eu': 1, 'gbp': 1.2, 'cad': 0.7}
+        self.rates['gbp'] = {'usd': 0.8, 'rur': 0.009, 'eu': 0.85, 'gbp': 1, 'cad': 0.6}
+        self.rates['cad'] = {'usd': 1.4, 'rur': 0.015, 'eu': 1.5, 'gbp': 1.7, 'cad': 1}
 
     def __str__(self) -> str:
         return str(self.currencies)
@@ -36,6 +36,21 @@ class Wallet:
         stack = 0
         for currency in self.currencies:
             stack += self.currencies[currency] * self.rates[goods_currency][currency]
-            if stack >= price:
-                return True
-            return False
+        if stack >= price:
+            return True
+        return False
+
+    def stack(self, to_currency: str) -> int:
+        stack = 0
+        for currency in self.currencies:
+            stack += self.currencies[to_currency] * self.rates[to_currency][currency]
+        return stack
+
+
+
+
+
+
+
+
+
